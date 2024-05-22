b96c7ebe466e639d8e7b4db9e185ff9f159a02db
Randal Andrade Nunes <randal.andrade@hotmail.com>
2024-05-22 10:58:55 -0400

    test: add test o rank next method

--- tests/rank_tests.rs
+++ tests/rank_tests.rs
@@ -12,3 +12,17 @@ fn test_rank_order() {
     assert!(Rank::Ace < Rank::Two);
     assert!(Rank::Two < Rank::Three);
 }
+
+#[test]
+fn test_rank_next() {
+    assert_eq!(Rank::Four.next(), Rank::Five);
+    assert_eq!(Rank::Five.next(), Rank::Six);
+    assert_eq!(Rank::Six.next(), Rank::Seven);
+    assert_eq!(Rank::Seven.next(), Rank::Queen);
+    assert_eq!(Rank::Queen.next(), Rank::Jack);
+    assert_eq!(Rank::Jack.next(), Rank::King);
+    assert_eq!(Rank::King.next(), Rank::Ace);
+    assert_eq!(Rank::Ace.next(), Rank::Two);
+    assert_eq!(Rank::Two.next(), Rank::Three);
+    assert_eq!(Rank::Three.next(), Rank::Four);
+}
