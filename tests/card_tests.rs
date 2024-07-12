e42570751b8ea87d67abf389457c866a1410ba97
Randal Andrade Nunes <randal.andrade@hotmail.com>
2024-07-12 19:40:06 +0000

    test: add compare cards and compare cards with manilha tests to card struct

--- tests/card_tests.rs
+++ tests/card_tests.rs
@@ -8,3 +8,29 @@ fn test_card_creation() {
     assert_eq!(card.rank, Rank::Ace, "Card rank should be Ace");
     assert_ne!(card.suit, Suit::Hearts, "Card suit should not be Hearts");
 }
+
+#[test]
+fn test_compare_cards() {
+    let card1 = Card::new(Suit::Clubs, Rank::Four);
+    let card2 = Card::new(Suit::Clubs, Rank::Five);
+    let card3 = Card::new(Suit::Clubs, Rank::Six);
+    let card4 = Card::new(Suit::Clubs, Rank::Seven);
+    let flip_card = Card::new(Suit::Clubs, Rank::Queen);
+
+    let cards = vec![card1, card2, card3, card4];
+
+    assert_eq!(Card::compare_cards(&cards, &flip_card), &card4);
+}
+
+#[test]
+fn test_compare_cards_with_manilha() {
+    let card1 = Card::new(Suit::Clubs, Rank::Four);
+    let card2 = Card::new(Suit::Clubs, Rank::Five);
+    let card3 = Card::new(Suit::Clubs, Rank::Six);
+    let card4 = Card::new(Suit::Clubs, Rank::Seven);
+    let flip_card = Card::new(Suit::Clubs, Rank::Queen);
+
+    let cards = vec![card1, card2, card3, card4];
+
+    assert_eq!(Card::compare_cards(&cards, &flip_card), &card4);
+}
