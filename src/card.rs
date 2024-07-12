46b93c823d187ab8d24168f4a3ca5131291e8121
Randal Andrade Nunes <randal.andrade@hotmail.com>
2024-07-12 19:37:47 +0000

    feat: add compare card function to card struct

--- src/card.rs
+++ src/card.rs
@@ -10,4 +10,10 @@ impl Card {
     pub fn new(suit: Suit, rank: Rank) -> Self {
         Card { suit, rank }
     }
+
+    pub fn compare_cards<'a>(cards: &'a [Card], flip_card: &Card) -> &'a Card {
+        let manilha_card = flip_card.rank.next();
+        println!("Manilha card: {:?}", manilha_card);
+        cards.iter().max_by(|a, b| a.rank.cmp(&b.rank)).unwrap()
+    }
 }
