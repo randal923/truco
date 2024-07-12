2896ad212acedb8901fe13571a91e1f3301a8af3
Randal Andrade Nunes <randal.andrade@hotmail.com>
2024-07-12 20:42:19 +0000

    feat: add nice card formatting

--- src/card.rs
+++ src/card.rs
@@ -16,4 +16,45 @@ impl Card {
         println!("Manilha card: {:?}", manilha_card);
         cards.iter().max_by(|a, b| a.rank.cmp(&b.rank)).unwrap()
     }
+
+    pub fn format_card(&self) -> String {
+        let rank = Card::rank_to_symbol(&self.rank);
+        let suit = Card::suit_to_symbol(&self.suit);
+
+        let card_str = format!(
+            "╭──────╮\n\
+             │{}     │\n\
+             │  {}   │\n\
+             │      │\n\
+             ╰──────╯\n\
+             ",
+            rank, suit
+        );
+
+        card_str.to_string()
+    }
+
+    fn rank_to_symbol(rank: &Rank) -> &str {
+        match rank {
+            Rank::Ace => "A",
+            Rank::Two => "2",
+            Rank::Three => "3",
+            Rank::Four => "4",
+            Rank::Five => "5",
+            Rank::Six => "6",
+            Rank::Seven => "7",
+            Rank::Jack => "J",
+            Rank::Queen => "Q",
+            Rank::King => "K",
+        }
+    }
+
+    fn suit_to_symbol(suit: &Suit) -> &str {
+        match suit {
+            Suit::Hearts => "♥",
+            Suit::Diamonds => "♦",
+            Suit::Clubs => "♣",
+            Suit::Spades => "♠",
+        }
+    }
 }
